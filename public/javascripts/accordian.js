f92ed5361e3fe23bd41caaa96fd85e40d1fe74ab
Klint Tiley <kctiley@gmail.com>
2015-10-16 14:53:08 -0600

    Now uses ajax to show tasks from database.  Next feature...include Add Task

--- public/javascripts/accordian.js
+++ public/javascripts/accordian.js
@@ -2,54 +2,96 @@ $(document).ready(function(){
 
   console.log("Document loaded...")
 
-  $(function() {
-      $( "#tasks" ).accordion();
-      $( "#tasks li" ).draggable({
-        appendTo: "body",
-        helper: "clone"
-      });
-      $( "#plan ol" ).droppable({
-        activeClass: "ui-state-default",
-        hoverClass: "ui-state-hover",
-        accept: ":not(.ui-sortable-helper)",
-        drop: function( event, ui ) {
-          $( this ).find( ".placeholder" ).remove();
-          $( "<li></li>" ).text( ui.draggable.text() ).appendTo( this );
+  // //Cache orders, name, and drink
+  // var $orders = $('#listOfOrders');
+  // var $inputName = $('#inputName');
+  // var $inputDrink = $('#inputDrink');
+
+  var $health = $('#health');
+  var $wealth = $('#wealth');
+  var $soul = $('#soul');
+
+  //Show database orders on page load
+  $.ajax({
+    type: 'GET',
+    url: '/tasks',
+    success: function(allOfTheTasks){
+      $.each(allOfTheTasks, function(i, each){
+        if(each.category == 'health'){
+          $health.append('<li>' + each.description + '</li>');
         }
-      }).sortable({
-        items: "li:not(.placeholder)",
-        sort: function() {
-          // gets added unintentionally by droppable interacting with sortable
-          // using connectWithSortable fixes this, but doesn't allow you to customize active/hoverClass options
-          $( this ).removeClass( "ui-state-default" );
+        if(each.category == 'wealth'){
+          $wealth.append('<li>' + each.description + '</li>');
         }
-      });
-
-
-      $( "#plan li" ).draggable({
-        appendTo: "body",
-        helper: "clone"
-      });
-
-      $( "#tasks ul" ).droppable({
-        activeClass: "ui-state-default",
-        hoverClass: "ui-state-hover",
-        accept: ":not(.ui-sortable-helper)",
-        drop: function( event, ui ) {
-          $( this ).find( ".placeholder" ).remove();
-          $( "<li></li>" ).text( ui.draggable.text() ).appendTo( this );
+        if(each.category == 'soul'){
+          $soul.append('<li>' + each.description + '</li>');
         }
-      }).sortable({
-        items: "li:not(.placeholder)",
-        sort: function() {
-          // gets added unintentionally by droppable interacting with sortable
-          // using connectWithSortable fixes this, but doesn't allow you to customize active/hoverClass options
-          $( this ).removeClass( "ui-state-default" );
-        }
-      });
+      })
 
-
+    //Accordion functionality, with drag/drop/sort ***********************
+    $(function() {
+        $( "#tasks" ).accordion();
+        $( "#tasks li" ).draggable({
+          appendTo: "body",
+          helper: "clone"
+        });
+        $( "#plan ol" ).droppable({
+          activeClass: "ui-state-default",
+          hoverClass: "ui-state-hover",
+          accept: ":not(.ui-sortable-helper)",
+          drop: function( event, ui ) {
+            $( this ).find( ".placeholder" ).remove();
+            $( "<li></li>" ).text( ui.draggable.text() ).appendTo( this );
+            $(ui.draggable).remove(); //remove from list
+          }
+        }).sortable({
+          items: "li:not(.placeholder)",
+          sort: function() {
+            // gets added unintentionally by droppable interacting with sortable
+            // using connectWithSortable fixes this, but doesn't allow you to customize active/hoverClass options
+            $( this ).removeClass( "ui-state-default" );
+          }
+        });
     });
+
+    //THIS ADDS ABILITY TO DRAG FROM PLAN  TO TASKS
+    $(function() {
+        
+        $( "#plan li" ).draggable({
+          appendTo: "body",
+          helper: "clone"
+        });
+        $( "#tasks ul" ).droppable({
+          activeClass: "ui-state-default",
+          hoverClass: "ui-state-hover",
+          accept: ":not(.ui-sortable-helper)",
+          drop: function( event, ui ) {
+            $( this ).find( ".placeholder" ).remove();
+            $( "<li></li>" ).text( ui.draggable.text() ).appendTo( this );
+            $(ui.draggable).remove(); //remove from list
+          }
+        }).sortable({
+          items: "li:not(.placeholder)",
+          sort: function() {
+            // gets added unintentionally by droppable interacting with sortable
+            // using connectWithSortable fixes this, but doesn't allow you to customize active/hoverClass options
+            $( this ).removeClass( "ui-state-default" );
+          }
+        });
+      });
+
+
+    },
+    error: function(){
+      alert('Error loading orders')
+    }
+  })
+ 
+
+
+
+
+
 
 
 })
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var db = require('monk')('localhost/jquery-ui-ajax-to-do-list');
+var taskCollection = db.get('taskCollection')
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'To Do List' });
+  taskCollection.find({}, function(err, record){
+    res.render('index', { title: 'To Do List', allTasks: record });
+  })
+});
+
+router.get('/tasks', function(req, res) {
+    taskCollection.find({},{},function(e,docs){
+        res.json(docs);
+    });
 });
 
 module.exports = router;
